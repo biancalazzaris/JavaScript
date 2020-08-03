4950500d00ccc61310a07aff42157ab9c687d8d4
biancalazzaris <bianca.lazzaris@gmail.com>
2020-08-02 22:45:24 -0300

    .

---
+++ BiancaLazzaris-Atividade1.js
@@ -0,0 +1,47 @@
+Qual o tipo de dado no JavaScript para textos?
+[ ] Text
+[ ] bigText
+[X] string
+
+Qual o tipo de dado no JavaScript para números?
+[ ] int
+[X] number
+[ ] integer
+
+Um dado true, faz parte de qual tipo de dados em JavaScript?
+[ ] positive
+[X] boolean
+[ ] choice
+
+Quais são os formatos possivéis aceitos para valores do tipo String? Dê exemplos usando o console.log;
+
+console.log (`Tipo de string, bjs`)
+
+console.log('Bianca' + ' ' + 'Lazzaris');
+
+console.log ("Olar mundo!")
+
+Quais são os formatos possivéis aceitos para valores do tipo Number? Dê exemplos usando o console.log;
+
+console.log(10);
+console.log(12.8);
+console.log(-50);
+
+Escreva três comparações possivéis com Boolean? Dê exemplos usando o console.log. Lembre dos operadores de comparação (maior, menor, igual e diferente);
+
+console.log(10 > 2);
+console.log(8 < 5);
+console.log(1 == '1');
+
+Escreva três comparações possivéis com operadores lógicos? Dê exemplos usando o console.log. Lembre dos operadores de comparação lógicos (AND, OR e NOT);
+
+console.log(true && true);
+console.log(true && false);
+console.log(false || false);
+console.log(!true);
+
+Vimos que existe o tipo NAN, faça 03 exemplos que possamos obter esse retorno, usando o console.log.
+
+console.log('b' +  10);
+console.log(100 * `olá`);
+console.log("js" / 50);
